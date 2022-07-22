ff29990afc804a1875544ea22aac65c7aa892ba9
xu13527711640 <jxu001@odu.edu>
2022-07-21 20:48:50 -0400

    Delete GitLab2 directory

--- GitLab2/src/Interval.java
+++
@@ -1,6 +0,0 @@
-public class Interval {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
-//This is from testBranch
--- GitLab2/src/test/java/App.java
+++
@@ -1,7 +0,0 @@
-package test.java;
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
-//This from master 
